5c753a1a3cbd6598e55ba32bc06cc73c66e027b3
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-08 16:56:00 -0600

    Add Layout

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,16 +7,81 @@
 
 import SwiftUI
 
+var optionToWin = [
+    "🪨": "📄",
+    "📄": "✂️",
+    "✂️": "🪨"
+]
+
+struct IconOptionButton: View {
+    var content: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(content, action: action)
+            .font(.largeTitle)
+            .padding(15)
+            .background(Color.secondary.opacity(0.2))
+            .clipShape(
+                RoundedRectangle(cornerRadius: 10)
+            ).overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.secondary, style: StrokeStyle(lineWidth: 5, dash: [10]))
+            )
+    }
+}
+
 struct ContentView: View {
+    @State private var letTheApp = "win"
+    @State private var appChoice = "📄"
+    
+    var choices = ["🪨", "📄", "✂️"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Rock, Paper, Scissors")
+            Spacer()
+            VStack {
+                HStack {
+                    Text("Q1").font(.title3)
+                    Spacer()
+                    Text("Score: 10")
+                        .font(.title3)
+                }
+                VStack(spacing: 10) {
+                    Text("I choose:")
+                        .font(.title2)
+                        .bold()
+                    Text("🪨")
+                        .font(.largeTitle)
+                    Text(letTheApp == "win" ? "Let me win" : "I want to loose")
+                        .font(.title)
+                        .foregroundColor(letTheApp == "win" ? .green : .red)
+                        .bold()
+                }
+                .frame(maxWidth: .infinity)
+                .padding(30)
+                .background(Color.secondary.opacity(0.2))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .padding(30)
+        
+            
+            Spacer()
+
+            HStack(spacing: 20) {
+                ForEach(choices, id: \.self) { choice in
+                    IconOptionButton(content: choice) {
+//                        evaluateAnswer(with: choice)
+                    }
+                }
+            }
         }
         .padding()
+
     }
+    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
